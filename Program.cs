5bdd4f69b4832f99e79b32cb0dad3e1cff38f5f6
Sheryl Acree <sacree@tql.com>
2024-09-06 10:35:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EDI_X12_Parser {
+    internal class Program {
+        static void Main(string[] args) {
+            string ediFilename = @"c:\Users\Sheryl.Acree\Downloads\204_4819_RXOLogisticsFLSmidth.edi";
+            string ediFileContents = File.ReadAllText(ediFilename);
+
+            Console.WriteLine(ediFileContents);
+
+            Console.WriteLine("\n\n Press enter to end;");
+            Console.ReadLine();
+        }
+   
+    }
+
+}
