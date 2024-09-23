0de75f6396593eef7a5c0a4a44f13f607bc20f3c
Sheryl Acree <sacree@tql.com>
2024-09-23 07:19:50 -0400

    This is the code from the exercise example

--- Program.cs
+++ Program.cs
@@ -6,14 +6,58 @@ using System.Threading.Tasks;
 using System.IO;
 
 namespace EDI_X12_Parser {
-    internal class Program {
-        static void Main(string[] args) {
-            string ediFilename = @"c:\Users\Sheryl.Acree\Downloads\204_4819_RXOLogisticsFLSmidth.edi";
+     class Program {
+        static void Main(string[]args) {
+            string ediFilename = @"\\tql.com\shares\RedirectedFolders\Sheryl.Acree\My Documents\Udemy EDI Training Documents\Sample_850_01_Orig.edi.txt";
+           //insert any file name above
             string ediFileContents = File.ReadAllText(ediFilename);
 
             Console.WriteLine(ediFileContents);
 
-            Console.WriteLine("\n\n Press enter to end;");
+            string elementSeperator = ediFileContents.Substring(103,1);
+            //add the position # of the file's element seperator here, this one is an asterisk, for C# it will be position # minus 1            
+            string lineSeperator = ediFileContents.Substring(105, 1);
+            //add the position # of the file's line/segment seperator here, this one is a tilde,  for C# it will be position # minus 1  
+
+            ediFileContents = ediFileContents.Replace("\r", "").Replace("\n", "");//replace the carriage return and line feeds with nothing
+
+            
+            Console.WriteLine("elementSeperator = " + elementSeperator);
+            Console.WriteLine("lineSeperator = " + lineSeperator );
+
+            string[] lines = ediFileContents.Split(char.Parse(lineSeperator));
+            Console.WriteLine("Number of lines = " + lines.Length);
+            
+
+            foreach (string line in lines) 
+                {
+               
+                Console.WriteLine(line);
+                string[] elements = line.Split(char.Parse(elementSeperator));
+                int loopCounter = 0;
+                string segment = "";
+                string elNum = "";
+                string elName = "";
+
+                foreach (string el in elements) 
+                {
+                    if (loopCounter == 0)
+                        {
+                    segment = el;
+                    }
+                    else {
+                        elNum = loopCounter.ToString("D2");
+                        elName = segment + elNum;
+                    Console.WriteLine( elName + " = " + el);
+                    }
+                    
+                    loopCounter++;
+                }
+           
+            
+            }
+
+            Console.WriteLine("\n\n Press enter to end;");//to keep the data on screen
             Console.ReadLine();
         }
    
